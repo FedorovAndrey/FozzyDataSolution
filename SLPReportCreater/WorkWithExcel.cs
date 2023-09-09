aed55b6f8e6aba40e71a3956ff76daf471aca50d
Andrey Fedorov <an200478@outlook.com>
2023-09-09 20:17:01 +0300

    Generate report from date to date

--- SLPReportCreater/WorkWithExcel.cs
+++ SLPReportCreater/WorkWithExcel.cs
@@ -2,6 +2,7 @@
 using NLog;
 using NLog.Extensions.Logging;
 using OfficeOpenXml;
+using OfficeOpenXml.Table.PivotTable;
 using SLPDBLibrary;
 using SLPHelper;
 using System.Drawing;
@@ -11,11 +12,13 @@ namespace SLPReportCreater
     public class WorkWithExcel
     {
         private int regionId;
+
+        private DateTime dateTime_Begin;
+        private DateTime dateTime_End;
+
         Microsoft.Extensions.Logging.ILogger logger ;
-        private ReportType type;
-        private DateTime datetimeReportBegin;
-        private DateTime datetimeReportEnd;
-        private List<tbRegions> regions;
+        ExcelPackage excel;
+
 
         public WorkWithExcel(int regonId)
         {
@@ -25,21 +28,73 @@ namespace SLPReportCreater
 
             this.regionId = regonId;
         }
-        
-
         public void Generate()
         {
             logger.LogInformation("Report generation for the region ID : " + regionId.ToString());
-            for (int i = 0; i < 500; i++)
+            try
             {
+                Task.Factory.StartNew(() => GenerateReport(ReportType.Day));
+                Task.Factory.StartNew(() => GenerateReport(ReportType.Week));
+                Task.Factory.StartNew(() => GenerateReport(ReportType.Month));
+                Task.Factory.StartNew(() => GenerateReport(ReportType.Year));
+
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+                {
+                    //Task.Factory.StartNew(() => GenerateReport(ReportType.Week));
+                }
+                if (DateTime.Now.Day == 1)
+                {
+                    //Task.Factory.StartNew(() => GenerateReport(ReportType.Month));
+                }
+                if (DateTime.Now.DayOfYear == 1)
+                {
+                    //Task.Factory.StartNew(() => GenerateReport(ReportType.Year));
+                }
+
             }
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex.Message);
+            }
+            
+
             logger.LogInformation("End report generation for the region ID" + regionId.ToString());
         }
-
-        private void GenerateReportByRegion(int region) 
+        private void GenerateReport(ReportType reportType)
         {
 
+
+            logger.LogInformation("A task is started to generate the report : " + reportType.ToString());
+
+            switch (reportType)
+            {
+                case ReportType.Day:
+                    dateTime_Begin = DateTime.Today.AddDays(-1);
+                    dateTime_End = DateTime.Today;
+                    break;
+                case ReportType.Week:
+                    dateTime_Begin = DateTime.Today.AddDays(-7).AddDays((-1) * (int)(DateTime.Today.DayOfWeek - 1));
+                    dateTime_End = DateTime.Today.AddDays((-1) * (int)(DateTime.Today.DayOfWeek - 1)); 
+                    break;
+                case ReportType.Month:
+                    dateTime_Begin = new DateTime(DateTime.Today.Year, DateTime.Today.AddMonths(-1).Month, 1);
+                    dateTime_End = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                    break;
+                case ReportType.Year:
+                    dateTime_Begin = new DateTime(DateTime.Today.AddYears(-1).Year, 1, 1);
+                    dateTime_End = new DateTime(DateTime.Today.Year, 1, 1);
+                    break;
+
+            }
+
+            #region 
+
+            #endregion
+
+
+            logger.LogInformation("The task for generating the report is complete : " + reportType.ToString());
         }
+
         private bool GenerateBranchReportTemplate(ref ExcelPackage package, string name)
         {
             bool bResult = false;
